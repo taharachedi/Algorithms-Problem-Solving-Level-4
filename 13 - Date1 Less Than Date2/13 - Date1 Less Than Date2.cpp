54fa41ec291cdb07e862692171b93f4fc80d8dd2
Taha Rachedi <168340845+taharachedi@users.noreply.github.com>
2024-11-02 16:53:01 +0100

    Update 13 - Date1 Less Than Date2.cpp

--- 13 - Date1 Less Than Date2/13 - Date1 Less Than Date2.cpp
+++ 13 - Date1 Less Than Date2/13 - Date1 Less Than Date2.cpp
@@ -54,8 +54,11 @@ stDate Read_Full_Date() {
 
 bool is_Date1_Less_Than_Date2(stDate Date1, stDate Date2) {
 
-    return (Date1.Year < Date2.Year) ? true : ((Date1.Month < Date2.Month) ? true : ((Date1.Day < Date2.Day) ? true : false));
+    return (Date1.Year < Date2.Year) ? true :
+        ((Date1.Year == Date2.Year) ? ((Date1.Month < Date2.Month) ? true :
+            ((Date1.Month == Date2.Month) ? Date1.Day < Date2.Day : false) ) : false);
 }
+
 
 
 int main()
